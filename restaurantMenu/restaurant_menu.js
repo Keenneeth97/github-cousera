8a4c92b42348384fbf401957b5e564c26f590641
Keenneeth97 <espinalkenneth2@gmail.com>
2026-09-23 08:01:51 -0400

    Working with Array Iteration for Restaurant Menu List

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -13,4 +13,4 @@ document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
 let dessertItem = '';
 for (let i = 0; i < dessertMenu.length; i++) {
     dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
-document.getElementById('dessertMenuItems').innerHTML = dessertItem;)
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
