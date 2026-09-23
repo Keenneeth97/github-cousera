8d8dc7fb6a37bdb34c67caa226ca0dc48056ef2c
Keenneeth97 <espinalkenneth2@gmail.com>
2026-09-23 08:04:37 -0400

    Working with Array Iteration for Restaurant Menu List Task

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak- $24', 'Pasta- $20', 'Burger- $22', 'Salmon- $26'];
+const dessertMenu = ['Cake- 10$', 'Ice Cream- $8', 'Pudding- $10', 'Fruit Salad- $8'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
