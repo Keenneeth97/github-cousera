a56e9534d3733b81f151e19a1c958f976260e86e
Keenneeth97 <espinalkenneth2@gmail.com>
2026-09-25 09:56:24 -0400

    Create Book Management System using Objects Task

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -26,7 +26,8 @@ let books = [];
             <p><strong>Author Name:</strong> ${book.authorName}</p>
             <p><strong>Book Description:</strong> ${book.bookDescription}</p>
             <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-            <button onclick="editbook(${index})">Edit</button>`
+            <button onclick="editbook(${index})">Edit</button> 
+            <button onclick="deletebook(${index})">Delete</button>`
         );
         document.getElementById('books').innerHTML = booksDiv.join('');
     }
@@ -40,6 +41,12 @@ let books = [];
         books.splice(index, 1); // Remove old entry
         showbooks(); // Refresh list
       }
+
+    function deletebook(index) {
+        const book = books[index];
+        books.splice(index, 1); // Remove old entry
+        showbooks(); // Refresh list
+    }
 
     function clearInputs() {
         document.getElementById('bookName').value = '';
